fe68dbe202237198fe6d3c90e2cb720a2e18cf6c
gtiku <giyonatiku@gmail.com>
2022-12-11 15:53:10 -0800

    comment form functionality

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -12,7 +12,7 @@ commentsSection.appendChild(header);
 // -------------------------------------------------------------
 
 // COMMENT FORM DIV
-let form = document.createElement('div');
+let form = document.createElement('form');
 form.classList.add('comments__form');
 commentsSection.appendChild(form);
 
@@ -30,6 +30,7 @@ form.appendChild(inputsDiv);
 // NAME LABEL
 let nameLabel = document.createElement('label');
 nameLabel.classList.add('comments__form-name');
+nameLabel.for = 'name';
 nameLabel.innerText = 'NAME'
 inputsDiv.appendChild(nameLabel);
 
@@ -37,7 +38,7 @@ inputsDiv.appendChild(nameLabel);
 let nameInput = document.createElement('input');
 nameInput.classList.add('comments__form-name');
 nameInput.type ='text';
-nameInput.for ='name';
+nameInput.name ='name';
 nameInput.placeholder = 'Enter your name'
 nameInput.required = true;
 inputsDiv.appendChild(nameInput);
@@ -45,6 +46,7 @@ inputsDiv.appendChild(nameInput);
 // COMMENT LABEL
 let commentLabel = document.createElement('label');
 commentLabel.classList.add('comments__form-text');
+commentLabel.for = 'comment';
 commentLabel.innerText = 'COMMENT';
 inputsDiv.appendChild(commentLabel);
 
@@ -65,46 +67,60 @@ button.innerText = 'COMMENT';
 inputsDiv.appendChild(button);
 
 
+
 // -------------------------------------------------------------
-// COMMENTS DISPLAY
+// COMMENT DATA
 // -------------------------------------------------------------
 
 commentData = [
     {
         name: 'Connor Walton',
         date: '02/17/2021',
-        comment: `This is art. This is inexplicable magic expressed 
-        in the purest way, everything that makes up this majestic work 
-        deserves reverence. Let us appreciate this for what it is and 
-        what it contains.`,
+        comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
     },
     {
         name: 'Emilie Beach',
         date: '01/09/2021',
-        comment: `I feel blessed to have seen them in person. What a show! 
-        They were just perfection. If there was one day of my life I 
-        could relive, this would be it. What an incredible day.`,
+        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
     },
     {
         name: 'Miles Acosta',
         date: '12/20/2020',
-        comment: `I can't stop listening. Every time I hear one of their 
-        songs - the vocals - it gives me goosebumps. Shivers straight 
-        down my spine. What a beautiful expression of creativity. Can't 
-        get enough.`,
+        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
     },
 ]
 
 
+
+
+// -------------------------------------------------------------
+// DISPLAY COMMENTS FUNCTION
+// -------------------------------------------------------------
+
+
+
+// BUILD COMMENTS DISPLAY DIV
+let allCommentsBox = document.createElement('div');
+allCommentsBox.classList.add('comments__all-comments');
+allCommentsBox.innerHTML = '';
+commentsSection.appendChild(allCommentsBox);
+
+
+
 displayComments = (commentArray) => {
 
+    // CLEAR COMMENTS
+    allCommentsBox.innerHTML = '';
+
+    // REBUILD COMMENTS 
     commentArray.forEach(comment => {
 
 
         // COMMENT BOX
         let commentBox = document.createElement('div');
         commentBox.classList.add('comments__box');
-        commentsSection.appendChild(commentBox);
+        commentBox.innerHTML = '';
+        allCommentsBox.appendChild(commentBox);
 
         // AVATAR
         let avatar = document.createElement('div');
@@ -139,9 +155,6 @@ displayComments = (commentArray) => {
         commentText.innerText = comment.comment;
         textDisplay.appendChild(commentText);
 
-
-
-
     });
 }
 
@@ -149,7 +162,35 @@ displayComments(commentData);
 
 
 
-
 // -------------------------------------------------------------
 // FORM LOGIC
 // -------------------------------------------------------------
+
+const submit = document.querySelector('form');
+
+submit.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+    
+    const name = event.target.name.value;
+    const comment = event.target.comment.value;
+    const date = new Date();
+
+
+    const newEntry = {
+        name: name,
+        date: date.toLocaleDateString(),
+        comment: comment,
+    };
+
+
+    commentData.unshift(newEntry);
+
+    displayComments(commentData);
+
+    event.target.reset();
+
+    
+})
+
+
