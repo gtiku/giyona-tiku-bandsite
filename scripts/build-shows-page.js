65ba7458997f4797a26f9c5f9c1e8bdeeb7fdf12
gtiku <giyonatiku@gmail.com>
2022-12-11 22:32:49 -0800

    overall styling

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,172 +1,166 @@
-const showsSection = document.querySelector('.shows');
+const showsSection = document.querySelector(".shows");
 
 // SECTION HEADER
-let header = document.createElement('h2');
-header.classList.add('shows__header');
-header.classList.add('section__header');
-header.innerText = 'Shows';
+let header = document.createElement("h2");
+header.classList.add("shows__header");
+header.classList.add("section__header");
+header.innerText = "Shows";
 showsSection.appendChild(header);
 
 // -------------------------------------------------------------
 // SHOWS LAYOUT
 // -------------------------------------------------------------
 
-
 // TABLET/DESKTOP LABELS DIV
-let labels = document.createElement('div');
-labels.classList.add('shows__labels');
+let labels = document.createElement("div");
+labels.classList.add("shows__labels");
 showsSection.appendChild(labels);
 
 // DATE LABEL
-let dateLabel = document.createElement('p');
-dateLabel.classList.add('shows__label');
-dateLabel.classList.add('shows__label--date');
-dateLabel.innerText = 'DATE';
+let dateLabel = document.createElement("p");
+dateLabel.classList.add("shows__label");
+dateLabel.classList.add("shows__label--date");
+dateLabel.innerText = "DATE";
 labels.appendChild(dateLabel);
 
 // VENUE LABEL
-let venueLabel = document.createElement('p');
-venueLabel.classList.add('shows__label');
-venueLabel.classList.add('shows__label--venue');
-venueLabel.innerText = 'VENUE';
+let venueLabel = document.createElement("p");
+venueLabel.classList.add("shows__label");
+venueLabel.classList.add("shows__label--venue");
+venueLabel.innerText = "VENUE";
 labels.appendChild(venueLabel);
 
 // LOCATION LABEL
-let locationLabel = document.createElement('p');
-locationLabel.classList.add('shows__label');
-locationLabel.classList.add('shows__label--location');
-locationLabel.innerText = 'LOCATION';
+let locationLabel = document.createElement("p");
+locationLabel.classList.add("shows__label");
+locationLabel.classList.add("shows__label--location");
+locationLabel.innerText = "LOCATION";
 labels.appendChild(locationLabel);
-
 
 // -------------------------------------------------------------
 // SHOWS DISPLAY
 // -------------------------------------------------------------
 
 const showsData = [
-    {
-        date: 'Mon Sept 06 2021',
-        venue: 'Ronald Lane ',
-        location: 'San Francisco, CA',
-
-    },
-    {
-        date: 'Tue Sept 21 2021',
-        venue: 'Pier 3 East',
-        location: 'San Francisco, CA',
-
-    },
-    {
-        date: 'Fri Oct 15 202',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA',
-
-    },
-    {
-        date: 'Sat Nov 06 2021 ',
-        venue: 'Hyatt Agency ',
-        location: 'San Francisco, CA',
-
-    },
-    {
-        date: 'Fri Nov 26 2021',
-        venue: 'Moscow Center ',
-        location: 'San Francisco, CA',
-
-    },
-    {
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club ',
-        location: 'San Francisco, CA',
-
-    },
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Oct 15 202",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 06 2021 ",
+    venue: "Hyatt Agency ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center ",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club ",
+    location: "San Francisco, CA",
+  },
 ];
 
-
-
-
 listShows = (shows) => {
+  shows.forEach((show) => {
+    // WRAPPER WITH MOBILE BUTTON
+    let eventBlock = document.createElement("div");
+    eventBlock.classList.add("shows__event-block");
+    showsSection.appendChild(eventBlock);
 
-    shows.forEach(show => {
+    // CONTAINER WITH TABLET/DESKTOP BUTTON
+    let eventData = document.createElement("div");
+    eventData.classList.add("shows__event-data");
+    eventBlock.appendChild(eventData);
 
-        // WRAPPER WITH MOBILE BUTTON
-        let eventBlock = document.createElement('div');
-        eventBlock.classList.add('shows__event-block');
-        showsSection.appendChild(eventBlock);
+    // DATE CONTAINER
+    let dataBlockDate = document.createElement("div");
+    dataBlockDate.classList.add("shows__data-block");
+    eventData.appendChild(dataBlockDate);
 
-        // CONTAINER WITH TABLET/DESKTOP BUTTON
-        let eventData = document.createElement('div');
-        eventData.classList.add('shows__event-data');
-        eventBlock.appendChild(eventData);
+    let dateLabelMobile = document.createElement("p");
+    dateLabelMobile.classList.add("shows__label");
+    dateLabelMobile.classList.add("shows__label--date");
+    dateLabelMobile.classList.add("shows__label--mobile");
+    dateLabelMobile.innerText = "DATE";
+    dataBlockDate.appendChild(dateLabelMobile);
 
-        // DATE CONTAINER
-        let dataBlockDate = document.createElement('div');
-        dataBlockDate.classList.add('shows__data-block');
-        eventData.appendChild(dataBlockDate);
+    let date = document.createElement("p");
+    date.classList.add("shows__info");
+    date.classList.add("shows__info--date");
+    date.innerText = show.date;
+    dataBlockDate.appendChild(date);
 
-        let dateLabelMobile = document.createElement('p');
-        dateLabelMobile.classList.add('shows__label');
-        dateLabelMobile.classList.add('shows__label--date');
-        dateLabelMobile.classList.add('shows__label--mobile');
-        dateLabelMobile.innerText = 'DATE';
-        dataBlockDate.appendChild(dateLabelMobile);
+    // VENUE CONTAINER
+    let dataBlockVenue = document.createElement("div");
+    dataBlockVenue.classList.add("shows__data-block");
+    eventData.appendChild(dataBlockVenue);
 
-        let date = document.createElement('p');
-        date.classList.add('shows__info');
-        date.classList.add('shows__info--date');
-        date.innerText = show.date;
-        dataBlockDate.appendChild(date);
+    let venueLabelMobile = document.createElement("p");
+    venueLabelMobile.classList.add("shows__label");
+    venueLabelMobile.classList.add("shows__label--venue");
+    venueLabelMobile.classList.add("shows__label--mobile");
+    venueLabelMobile.innerText = "VENUE";
+    dataBlockVenue.appendChild(venueLabelMobile);
 
-        // VENUE CONTAINER
-        let dataBlockVenue = document.createElement('div');
-        dataBlockVenue.classList.add('shows__data-block');
-        eventData.appendChild(dataBlockVenue);
-       
+    let venue = document.createElement("p");
+    venue.classList.add("shows__info");
+    venue.innerText = show.venue;
+    dataBlockVenue.appendChild(venue);
 
-        let venueLabelMobile = document.createElement('p');
-        venueLabelMobile.classList.add('shows__label');
-        venueLabelMobile.classList.add('shows__label--venue');
-        venueLabelMobile.classList.add('shows__label--mobile');
-        venueLabelMobile.innerText = 'VENUE';
-        dataBlockVenue.appendChild(venueLabelMobile);
+    // LOCATION CONTAINER
+    let dataBlockLocation = document.createElement("div");
+    dataBlockLocation.classList.add("shows__data-block");
+    eventData.appendChild(dataBlockLocation);
 
-        let venue = document.createElement('p');
-        venue.classList.add('shows__info');
-        venue.innerText = show.venue;
-        dataBlockVenue.appendChild(venue);
+    let locationLabelMobile = document.createElement("p");
+    locationLabelMobile.classList.add("shows__label");
+    locationLabelMobile.classList.add("shows__label--location");
+    locationLabelMobile.classList.add("shows__label--mobile");
+    locationLabelMobile.innerText = "LOCATION";
+    dataBlockLocation.appendChild(locationLabelMobile);
 
-        // LOCATION CONTAINER
-        let dataBlockLocation = document.createElement('div');
-        dataBlockLocation.classList.add('shows__data-block');
-        eventData.appendChild(dataBlockLocation);
-       
+    let location = document.createElement("p");
+    location.classList.add("shows__info");
+    location.innerText = show.location;
+    dataBlockLocation.appendChild(location);
 
-        let locationLabelMobile = document.createElement('p');
-        locationLabelMobile.classList.add('shows__label');
-        locationLabelMobile.classList.add('shows__label--location');
-        locationLabelMobile.classList.add('shows__label--mobile');
-        locationLabelMobile.innerText = 'LOCATION';
-        dataBlockLocation.appendChild(locationLabelMobile);
+    let button = document.createElement("button");
+    button.classList.add("shows__button");
+    button.classList.add("shows__button--nomobile");
+    button.innerText = "BUY TICKETS";
+    eventData.appendChild(button);
 
-        let location = document.createElement('p');
-        location.classList.add('shows__info');
-        location.innerText = show.location;
-        dataBlockLocation.appendChild(location);
-
-        let button = document.createElement('button');
-        button.classList.add('shows__button');
-        button.classList.add('shows__button--nomobile');
-        button.innerText = 'BUY TICKETS';
-        eventData.appendChild(button);
-
-        let mobileButton = document.createElement('button');
-        mobileButton.classList.add('shows__button');
-        mobileButton.classList.add('shows__button--mobile');
-        mobileButton.innerText = 'BUY TICKETS';
-        eventBlock.appendChild(mobileButton);
-
-    });
-}
-
+    let mobileButton = document.createElement("button");
+    mobileButton.classList.add("shows__button");
+    mobileButton.classList.add("shows__button--mobile");
+    mobileButton.innerText = "BUY TICKETS";
+    eventBlock.appendChild(mobileButton);
+  });
+};
 
 listShows(showsData);
+
+// -------------------------------------------------------------
+// ONCLICK STYLING
+// -------------------------------------------------------------
+
+const selectedShow = document.querySelector(".shows__event-block");
+
+selectedShow.addEventListener("click", (event) => {
+  document.querySelectorAll('.shows__event-block').classList.remove('shows__event-block--selected');
+  selectedShow.classList.add("shows__event-block--selected");
+});
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,10 +1,10 @@
-const commentsSection = document.querySelector('.comments');
+const commentsSection = document.querySelector(".comments");
 
 // SECTION HEADER
-let header = document.createElement('h2');
-header.classList.add('comments__header');
-header.classList.add('section__header');
-header.innerText = 'Join the Conversation';
+let header = document.createElement("h2");
+header.classList.add("comments__header");
+header.classList.add("section__header");
+header.innerText = "Join the Conversation";
 commentsSection.appendChild(header);
 
 // -------------------------------------------------------------
@@ -12,185 +12,166 @@ commentsSection.appendChild(header);
 // -------------------------------------------------------------
 
 // COMMENT FORM DIV
-let form = document.createElement('form');
-form.classList.add('comments__form');
+let form = document.createElement("form");
+form.classList.add("comments__form");
 commentsSection.appendChild(form);
 
 // AVATAR
-let avatar = document.createElement('div');
-avatar.classList.add('comments__pfp-box');
-avatar.classList.add('comments__pfp-box--form');
+let avatar = document.createElement("div");
+avatar.classList.add("comments__pfp-box");
+avatar.classList.add("comments__pfp-box--form");
 form.appendChild(avatar);
 
 // INPUTS DIV
-let inputsDiv = document.createElement('div');
-inputsDiv.classList.add('comments__input');
+let inputsDiv = document.createElement("div");
+inputsDiv.classList.add("comments__input");
 form.appendChild(inputsDiv);
 
 // NAME LABEL
-let nameLabel = document.createElement('label');
-nameLabel.classList.add('comments__form-name');
-nameLabel.for = 'name';
-nameLabel.innerText = 'NAME'
+let nameLabel = document.createElement("label");
+nameLabel.classList.add("comments__form-name");
+nameLabel.for = "name";
+nameLabel.innerText = "NAME";
 inputsDiv.appendChild(nameLabel);
 
 // NAME INPUT
-let nameInput = document.createElement('input');
-nameInput.classList.add('comments__form-name');
-nameInput.type ='text';
-nameInput.name ='name';
-nameInput.placeholder = 'Enter your name'
+let nameInput = document.createElement("input");
+nameInput.classList.add("comments__form-name");
+nameInput.type = "text";
+nameInput.name = "name";
+nameInput.placeholder = "Enter your name";
 nameInput.required = true;
 inputsDiv.appendChild(nameInput);
 
 // COMMENT LABEL
-let commentLabel = document.createElement('label');
-commentLabel.classList.add('comments__form-text');
-commentLabel.for = 'comment';
-commentLabel.innerText = 'COMMENT';
+let commentLabel = document.createElement("label");
+commentLabel.classList.add("comments__form-text");
+commentLabel.for = "comment";
+commentLabel.innerText = "COMMENT";
 inputsDiv.appendChild(commentLabel);
 
 // COMMENT INPUT
-let commentInput = document.createElement('input');
-commentInput.classList.add('comments__form-text');
-commentInput.type = 'textarea';
-commentInput.name = 'comment';
-commentInput.placeholder = 'Add a new comment';
+let commentInput = document.createElement("input");
+commentInput.classList.add("comments__form-text");
+commentInput.type = "textarea";
+commentInput.name = "comment";
+commentInput.placeholder = "Add a new comment";
 commentInput.required = true;
 inputsDiv.appendChild(commentInput);
 
 // SUBMIT BUTTON
-let button = document.createElement('button');
-button.classList.add('comments__button');
-button.type = 'submit';
-button.innerText = 'COMMENT';
+let button = document.createElement("button");
+button.classList.add("comments__button");
+button.type = "submit";
+button.innerText = "COMMENT";
 inputsDiv.appendChild(button);
-
-
 
 // -------------------------------------------------------------
 // COMMENT DATA
 // -------------------------------------------------------------
 
 commentData = [
-    {
-        name: 'Connor Walton',
-        date: '02/17/2021',
-        comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-    },
-    {
-        name: 'Emilie Beach',
-        date: '01/09/2021',
-        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-    },
-    {
-        name: 'Miles Acosta',
-        date: '12/20/2020',
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    },
-]
-
-
-
+  {
+    name: "Connor Walton",
+    date: "02/17/2021",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
 
 // -------------------------------------------------------------
 // DISPLAY COMMENTS FUNCTION
 // -------------------------------------------------------------
 
-
-
 // BUILD COMMENTS DISPLAY DIV
-let allCommentsBox = document.createElement('div');
-allCommentsBox.classList.add('comments__all-comments');
-allCommentsBox.innerHTML = '';
+let allCommentsBox = document.createElement("div");
+allCommentsBox.classList.add("comments__all-comments");
+allCommentsBox.innerHTML = "";
 commentsSection.appendChild(allCommentsBox);
 
-
-
 displayComments = (commentArray) => {
+  // CLEAR COMMENTS
+  allCommentsBox.innerHTML = "";
 
-    // CLEAR COMMENTS
-    allCommentsBox.innerHTML = '';
+  // REBUILD COMMENTS
+  commentArray.forEach((comment) => {
+    // COMMENT BOX
+    let commentBox = document.createElement("div");
+    commentBox.classList.add("comments__box");
+    commentBox.innerHTML = "";
+    allCommentsBox.appendChild(commentBox);
 
-    // REBUILD COMMENTS 
-    commentArray.forEach(comment => {
+    // AVATAR
+    let avatar = document.createElement("div");
+    avatar.classList.add("comments__pfp-box");
+    commentBox.appendChild(avatar);
 
+    // TEXT DIV
+    let textDisplay = document.createElement("div");
+    textDisplay.classList.add("comments__text-display");
+    commentBox.appendChild(textDisplay);
 
-        // COMMENT BOX
-        let commentBox = document.createElement('div');
-        commentBox.classList.add('comments__box');
-        commentBox.innerHTML = '';
-        allCommentsBox.appendChild(commentBox);
+    // NAME-DATE DIV
+    let nameDateDiv = document.createElement("div");
+    nameDateDiv.classList.add("comments__name-date");
+    textDisplay.appendChild(nameDateDiv);
 
-        // AVATAR
-        let avatar = document.createElement('div');
-        avatar.classList.add('comments__pfp-box');
-        commentBox.appendChild(avatar);
+    // NAME
+    let name = document.createElement("p");
+    name.classList.add("comments__name");
+    name.innerText = comment.name;
+    nameDateDiv.appendChild(name);
 
-        // TEXT DIV
-        let textDisplay = document.createElement('div');
-        textDisplay.classList.add('comments__text-display');
-        commentBox.appendChild(textDisplay);
+    // DATE
+    let date = document.createElement("p");
+    date.classList.add("comments__date");
+    date.innerText = comment.date;
+    nameDateDiv.appendChild(date);
 
-        // NAME-DATE DIV
-        let nameDateDiv = document.createElement('div');
-        nameDateDiv.classList.add('comments__name-date');
-        textDisplay.appendChild(nameDateDiv);
-
-        // NAME
-        let name = document.createElement('p');
-        name.classList.add('comments__name');
-        name.innerText = comment.name;
-        nameDateDiv.appendChild(name);
-
-        // DATE
-        let date = document.createElement('p');
-        date.classList.add('comments__date');
-        date.innerText = comment.date;
-        nameDateDiv.appendChild(date);
-
-        // COMMENT
-        let commentText = document.createElement('p');
-        commentText.classList.add('comments__comment');
-        commentText.innerText = comment.comment;
-        textDisplay.appendChild(commentText);
-
-    });
-}
+    // COMMENT
+    let commentText = document.createElement("p");
+    commentText.classList.add("comments__comment");
+    commentText.innerText = comment.comment;
+    textDisplay.appendChild(commentText);
+  });
+};
 
 displayComments(commentData);
-
-
 
 // -------------------------------------------------------------
 // FORM LOGIC
 // -------------------------------------------------------------
 
-const submit = document.querySelector('form');
+const submit = document.querySelector("form");
 
-submit.addEventListener('submit', (event) => {
-    
-    event.preventDefault();
-    
-    const name = event.target.name.value;
-    const comment = event.target.comment.value;
-    const date = new Date();
+submit.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const name = event.target.name.value;
+  const comment = event.target.comment.value;
+  const date = new Date();
 
-    const newEntry = {
-        name: name,
-        date: date.toLocaleDateString(),
-        comment: comment,
-    };
+  const newEntry = {
+    name: name,
+    date: date.toLocaleDateString(),
+    comment: comment,
+  };
 
+  commentData.unshift(newEntry);
 
-    commentData.unshift(newEntry);
+  displayComments(commentData);
 
-    displayComments(commentData);
-
-    event.target.reset();
-
-    
-})
-
-
+  event.target.reset();
+});
